efaecd2dfa445b0a5a5cb9ae393700b88de4216c
hedi72 <heditaieb72@gmail.com>
2024-04-21 11:43:21 +0100

    task

---
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development", // Add this line to set the mode to development
+};
