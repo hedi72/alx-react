f4948c90bbf123270e48def86879eb1071e8ae5b
hedi72 <heditaieb72@gmail.com>
2024-04-25 12:07:14 +0100

    task

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,7 +1,21 @@
-import immutable from 'immutable';
+import { Seq, fromJS } from "immutable";
 
-const { Seq } = immutable;
+const printBestStudents = (object) => {
+  const lazySeq = Seq(fromJS(object))
+    .filter((student) => student.get("score") >= 70)
+    .map((student) =>
+      student
+        .update(
+          "firstName",
+          (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1)
+        )
+        .update(
+          "lastName",
+          (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1)
+        )
+    );
 
-export default function printBestStudents() {
-    
-}
+  console.log(lazySeq.toJS());
+};
+
+export default printBestStudents;
